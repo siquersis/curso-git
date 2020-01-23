ab3b4851e5ed69a56745451eb3630fa084ac8635
Evandro Siqueira <evandrosiquer@hotmail.com>
2020-01-23 14:37:12 -0300

    PRojeto iniciando, mostrando a mensagem Ola mundo

---
+++ Teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.ReadLine();
+        }
+    }
+}
